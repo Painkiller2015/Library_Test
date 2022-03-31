6900dbc69397e81b37479a646cb6e227af66f520
Александр <Volosnikov.aleksandr2@gmail.com>
2022-03-31 08:43:06 +0500

    validation triangle

--- Figure.cs
+++ Figure.cs
@@ -48,6 +48,8 @@
 
             public Triangle(double side1, double side2, double side3)
             {
+                Validation(side1, side2, side3);
+
                 Side1 = side1;
                 Side2 = side2;
                 Side3 = side3;
@@ -57,6 +59,14 @@
                     Math.Pow(GetHypotenuse(), 2) == Math.Pow(Side1, 3) + Math.Pow(Side2, 1))
                 {
                     IsRectangular = true;
+                }
+            }
+
+            private static void Validation(double side1, double side2, double side3)
+            {
+                if (side1 + side2 <= side3 || side2 + side3 <= side1  || side3 + side1 <= side2)
+                {
+                    throw IncorrectTriangleExeption("Incorrect Triangle");
                 }
             }
 
---
+++ IncorrectTriangleExeption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    [Serializable]
+    public class IncorrectTriangleExeption : Exception
+    {
+        public IncorrectTriangleExeption(string message)
+        : base(message) { }
+    }
+}
